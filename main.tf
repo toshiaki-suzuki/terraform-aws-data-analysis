f0d7b3a71d4d37223ca5deed442c4f4904d466a8
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-11-12 00:13:07 +0900

    Add Redshift module

--- main.tf
+++ main.tf
@@ -4,5 +4,16 @@ module "s3-bucket" {
   bucket  = "data-lake-for-terraform-data-analysis"
 }
 
+module "redshift" {
+  source  = "terraform-aws-modules/redshift/aws"
+  version = "5.0.0"
 
+  cluster_identifier = "terraform-data-analysis-redshift-cluster"
+  node_type = "dc2.large"
+  number_of_nodes = 1
 
+  database_name          = var.database_name
+  master_username        = var.master_username
+  create_random_password = false
+  master_password        = var.master_password  
+}
---
+++ outputs.tf
@@ -0,0 +1,11 @@
+variable "database_name" {
+  type = string
+}
+
+variable "master_username" {
+  type = string
+}
+
+variable "master_password" {
+  type = string
+}
